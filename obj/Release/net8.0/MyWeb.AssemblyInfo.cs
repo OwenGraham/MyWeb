43000b02759137f3f7fa64ff4d564826863e0893
OwenGraham <owen.graham@outlook.com>
2024-03-18 18:53:38 +0000

    Azure setup and navbar changes
    Files added for hosting site with Azure. Icons folder added to wwwroot
    directory and credit page created with attributions to icon providers.
    Credit page linked in footer. Github link added to navbar.

--- obj/Release/net8.0/MyWeb.AssemblyInfo.cs
+++ obj/Release/net8.0/MyWeb.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWeb")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c55bc7cf8f23ac96ffd5174c70bee78cd784c713")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWeb")]
