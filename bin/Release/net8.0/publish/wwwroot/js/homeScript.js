79ab1841297e70b8d72605e9dbeb3854039c1dd0
OwenGraham <71776173+OwenGraham@users.noreply.github.com>
2024-10-01 10:16:45 +0100

    Delete bin directory

--- bin/Release/net8.0/publish/wwwroot/js/homeScript.js
+++
@@ -1,7 +0,0 @@
-var observer = new IntersectionObserver(function(entries){
-    if(entries[0].isIntersecting === true)
-        document.querySelector('#welcomeText').animate([{opacity: 0}, {opacity: 1}], 2000,)
-}, {threshold: [0]});
-
-var testy = document.querySelector('#welcome');
-observer.observe(testy);
