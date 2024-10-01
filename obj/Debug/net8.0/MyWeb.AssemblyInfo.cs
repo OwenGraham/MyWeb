aed7c825f6cbe6109f90b7fb4fac06b07201b2ac
Owen Graham <owen.graham@outlook.com>
2024-10-01 10:15:58 +0100

    added .vscode and bin/ and obj/ to gitignore

--- obj/Debug/net8.0/MyWeb.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918a56f89896ab6c605b379b1c4c2a8483df25c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0214536e7208d6376af6aed45777fc8dae63be91")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
